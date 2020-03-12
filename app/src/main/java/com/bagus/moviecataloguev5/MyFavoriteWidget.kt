ae2f74d1afd5e72552a9370d455d89a87f8df31f
bagusindrayana <bagusindrayana@users.noreply.github.com>
2020-03-12 08:55:56 +0800

    revisi widget

--- app/src/main/java/com/bagus/moviecataloguev5/MyFavoriteWidget.kt
+++ app/src/main/java/com/bagus/moviecataloguev5/MyFavoriteWidget.kt
@@ -5,15 +5,25 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.os.Binder
 import android.widget.RemoteViews
 import android.widget.Toast
 import androidx.core.net.toUri
+import com.bagus.moviecataloguev5.db.DatabaseContract
+import com.bagus.moviecataloguev5.helper.MappingHelper
+import com.bagus.moviecataloguev5.model.favorite.Favorite
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.AppWidgetTarget
 
 
 class MyFavoriteWidget : AppWidgetProvider() {
 
     companion object {
-
+        private var data = ArrayList<Favorite>()
+        private var cursor : Cursor? = null
         private const val TOAST_ACTION = "com.bagus.moviecataloguev5.TOAST_ACTION"
         const val EXTRA_ITEM = "com.bagus.moviecataloguev5.EXTRA_ITEM"
 
@@ -33,14 +43,56 @@ class MyFavoriteWidget : AppWidgetProvider() {
             val toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
             views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent)
 
+            getMovie(context)
+
+            if(data != null){
+                for(l in data){
+
+                    val awt: AppWidgetTarget = object : AppWidgetTarget(context.applicationContext, R.id.imageView, views, appWidgetId) {
+                        override fun onResourceReady(
+                            resource: Bitmap,
+                            transition: com.bumptech.glide.request.transition.Transition<in Bitmap>?
+                        ) {
+                            super.onResourceReady(resource, transition)
+                        }
+                    }
+
+                    var options = RequestOptions().placeholder(R.mipmap.ic_launcher_round).error(R.mipmap.ic_launcher_round)
+
+                    Glide.with(context.applicationContext).asBitmap().load("https://image.tmdb.org/t/p/w342/"+l.poster).apply(options).into(awt)
+                }
+            }
+
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
+
+        fun getMovie(mContext : Context){
+
+            if(cursor != null){
+                cursor?.close()
+            }
+            val identityToken = Binder.clearCallingIdentity()
+
+            cursor = mContext.contentResolver?.query(DatabaseContract.FavoriteColumns.CONTENT_URI, null, "MOVIE", null, null)
+
+            Binder.restoreCallingIdentity(identityToken)
+
+            data = MappingHelper.mapCursorToArrayList(cursor)
+
+
+        }
     }
+
+
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
+
+
             updateAppWidget(context, appWidgetManager, appWidgetId)
+
+
         }
     }
 
--- app/src/main/java/com/bagus/moviecataloguev5/StackRemoteViewsFactory.kt
+++ app/src/main/java/com/bagus/moviecataloguev5/StackRemoteViewsFactory.kt
@@ -2,57 +2,41 @@ package com.bagus.moviecataloguev5
 
 import android.content.Context
 import android.content.Intent
+import android.database.Cursor
 import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
+import android.os.Binder
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
 import androidx.core.os.bundleOf
 import com.bagus.moviecataloguev5.db.DatabaseContract
-import com.bagus.moviecataloguev5.db.FavoriteProvider.Companion.rom
 import com.bagus.moviecataloguev5.helper.MappingHelper
+import com.bagus.moviecataloguev5.model.favorite.Favorite
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.target.AppWidgetTarget
 
 internal class StackRemoteViewsFactory(private val mContext: Context) : RemoteViewsService.RemoteViewsFactory {
-    private val mWidgetItems = ArrayList<Bitmap>()
+    private var data = ArrayList<Favorite>()
+    private var cursor : Cursor? = null
 
     fun getMovie(){
-        val cursor = mContext.contentResolver?.query(DatabaseContract.FavoriteColumns.CONTENT_URI, null, "MOVIE", null, null)
 
-        val data = MappingHelper.mapCursorToArrayList(cursor)
-
-        if (data != null) {
-            for(l in data){
-                val options = RequestOptions()
-                    .centerCrop()
-                    .placeholder(R.mipmap.ic_launcher_round)
-                    .error(R.mipmap.ic_launcher_round)
-                Glide.with(mContext)
-                    .asBitmap()
-                    .load("https://image.tmdb.org/t/p/w342/"+l.poster)
-                    .apply(options)
-                    .into(object : CustomTarget<Bitmap>(){
-                        override fun onLoadCleared(placeholder: Drawable?) {
-
-                        }
-
-                        override fun onResourceReady(
-                            resource: Bitmap,
-                            transition: com.bumptech.glide.request.transition.Transition<in Bitmap>?
-                        ) {
-                            mWidgetItems.add(resource)
-
-                        }
-
-                    })
-            }
+        if(cursor != null){
+            cursor?.close()
         }
-        rom?.close()
+        val identityToken = Binder.clearCallingIdentity()
+
+        cursor = mContext.contentResolver?.query(DatabaseContract.FavoriteColumns.CONTENT_URI, null, "MOVIE", null, null)
+
+        Binder.restoreCallingIdentity(identityToken)
+
+        data = MappingHelper.mapCursorToArrayList(cursor)
+
+
     }
 
     override fun onCreate() {
-
+        //getMovie()
     }
 
     override fun onDestroy() {
@@ -65,11 +49,24 @@ internal class StackRemoteViewsFactory(private val mContext: Context) : RemoteVi
     }
 
 
-    override fun getCount(): Int = mWidgetItems.size
+    override fun getCount(): Int = data.size
 
     override fun getViewAt(position: Int): RemoteViews {
-        val rv = RemoteViews(mContext.packageName, R.layout.widget_item)
-        rv.setImageViewBitmap(R.id.imageView, mWidgetItems[position])
+        val rv = RemoteViews(mContext.packageName, R.layout.my_favorite_widget)
+        val awt: AppWidgetTarget = object : AppWidgetTarget(mContext.applicationContext, R.id.imageView, rv, getItemId(position).toInt()) {
+             override fun onResourceReady(
+                resource: Bitmap,
+                transition: com.bumptech.glide.request.transition.Transition<in Bitmap>?
+            ) {
+                super.onResourceReady(resource, transition)
+            }
+        }
+
+        var options = RequestOptions().placeholder(R.mipmap.ic_launcher_round).error(R.mipmap.ic_launcher_round)
+
+        Glide.with(mContext.applicationContext).asBitmap().load("https://image.tmdb.org/t/p/w342/"+data[position].poster).apply(options).into(awt)
+
+
 
         val extras = bundleOf(
             MyFavoriteWidget.EXTRA_ITEM to position
